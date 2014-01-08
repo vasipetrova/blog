3d0308767324369c3aee1ad6da45433d5b391b22
vasipetrova <vasi.petrova@yahoo.ca>
2014-01-07 23:10:29 -0700

    Wrote a migration to create a new users table
    Wrote a migration to create the users table. The table consists of the
    following columns:
    - username
    - email
    - password_hash
    - password_salt
    - user_photo
    timestamps - created_at and updated_at

---
+++ db/migrate/20140107105500_create_user.rb
@@ -0,0 +1,17 @@
+class CreateUser < ActiveRecord::Migration 
+	def self.up
+		create_table :users do |t|
+			t.column	:username, :string
+			t.column	:email, :string
+			t.column	:password_hash, :string
+			t.column	:password_salt,	:string
+			t.column	:user_photo, :string
+			
+			t.timestamps
+		end
+	end
+	
+	def self.down
+		drop_table :users
+	end
+end
